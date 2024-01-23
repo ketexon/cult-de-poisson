9ab2a0ee18f51cdbf2431d0f02e5e73f70cd2c46
Schuyler Henry <schenry823@gmail.com>
2024-01-23 14:42:26 -0800

    Journal can be looked at and exited

--- Assets/Objects/Player/PlayerItem.cs
+++ Assets/Objects/Player/PlayerItem.cs
@@ -89,6 +89,7 @@ public class PlayerItem : MonoBehaviour
         {
             float v = ctx.ReadValue<float>();
             newItemIndex = (Math.Sign(v) + EnabledItemIndex + items.Count) % items.Count;
+            Debug.Log(newItemIndex);
         }
 
         EnableItem(newItemIndex);
