ffdfd6fae8bc3ad174187d8657b73ceb4b6dc9d9
Schuyler Henry <schenry823@gmail.com>
2024-01-28 17:04:13 -0800

    Fish Info Dropdown mockup

--- Assets/Objects/Fish/FishSO.cs
+++ Assets/Objects/Fish/FishSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,43 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Fish", menuName = "Fish")]
 public class FishSO : ScriptableObject
 {
-    public string Name;
-    public GameObject InWaterPrefab;
-    public GameObject InHandPrefab;
-    public GameObject InBucketPrefab;
+	public string Name;
+	public GameObject InWaterPrefab;
+	public GameObject InHandPrefab;
+	public GameObject InBucketPrefab;
+	public FishInfo FishInfo;
 
-    void Reset()
-    {
-        InHandPrefab = FindUtil.QueryAsset<GameObject>()
-            .SiblingTo(this)
-            .NameContains("hand", insensitive: true)
-            .Execute();
+	void Reset()
+	{
+		InHandPrefab = FindUtil.QueryAsset<GameObject>()
+			.SiblingTo(this)
+			.NameContains("hand", insensitive: true)
+			.Execute();
 
-        InWaterPrefab = new FindUtil.FindAssetQuery<GameObject>()
-            .SiblingTo(this)
-            .NameContains("water", insensitive: true)
-            .Execute();
+		InWaterPrefab = new FindUtil.FindAssetQuery<GameObject>()
+			.SiblingTo(this)
+			.NameContains("water", insensitive: true)
+			.Execute();
 
-        InWaterPrefab = new FindUtil.FindAssetQuery<GameObject>()
-            .SiblingTo(this)
-            .NameContains("bucket", insensitive: true)
-            .Execute();
-    }
+		InWaterPrefab = new FindUtil.FindAssetQuery<GameObject>()
+			.SiblingTo(this)
+			.NameContains("bucket", insensitive: true)
+			.Execute();
+	}
+}
+
+
+[Serializable]
+public struct FishInfo
+{
+	public enum Depth { Shallow, Mid, Deep }
+
+	public float Weight;
+	[Min(0)] public int LengthFeet, LengthInches;
+	public Depth FishDepth;
+	public string CatchMethod;
+	public string FavoredHabitat;
+	public int CatchDifficulty;
+	public bool IsKeyFish;
+	public string Notes;
 }
---
+++ Assets/Objects/Items/Journal/UI Elements/SpeciesDropdown.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System;
+
+public class SpeciesDropdown : JournalUIElement, IPointerClickHandler
+{
+	const float DROPDOWN_HEIGHT = 360f;
+	const float TOLERANCE = 10f;
+
+	[SerializeField] FishSO fishSO;
+	[SerializeField] TMP_Text speciesLabel;
+	[SerializeField] GameObject dropdownPanel;
+	[SerializeField] TMP_Text weight, length, depth, catchMethod, favoredHabitat, catchDifficulty, isKeyFish, notes;
+
+	bool isOpen = false;
+	bool isInteractable = true;
+
+	void Awake()
+	{
+		//Setting all the text fields
+		speciesLabel.text = fishSO.Name;
+		weight.text = "Weight: " + fishSO.FishInfo.Weight + " lbs";
+		length.text = "Length: " + fishSO.FishInfo.LengthFeet + "' " + fishSO.FishInfo.LengthInches + "\"";
+		depth.text = "Depth: " + fishSO.FishInfo.FishDepth;
+		catchMethod.text = "Catch Method: " + fishSO.FishInfo.CatchMethod;
+		favoredHabitat.text = "Favored Habitat: " + fishSO.FishInfo.FavoredHabitat;
+		catchDifficulty.text = "Catch Difficulty: " + fishSO.FishInfo.CatchDifficulty;
+		isKeyFish.text = "Key Fish: " + fishSO.FishInfo.IsKeyFish;
+		notes.text = "Notes: " + fishSO.FishInfo.Notes;
+	}
+
+	//Implemented by IPointerClickHandler interface
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (!isInteractable) return;
+
+		if (!isOpen)
+		{
+			OpenDropdown();
+		}
+		else
+		{
+			CloseDropdown();
+		}
+	}
+
+	//Opens the species dropdown and displays the fish's data
+	public void OpenDropdown()
+	{
+		isOpen = true;
+		StartCoroutine(DoOpenDropdown());
+	}
+
+	public void CloseDropdown()
+	{
+		isOpen = false;
+		StartCoroutine(DoCloseDropdown());
+	}
+
+	IEnumerator DoOpenDropdown()
+	{
+		isInteractable = false;
+		dropdownPanel.SetActive(true);
+		yield return new WaitForSeconds(0.1f);
+
+		RectTransform rectTransform = dropdownPanel.GetComponent<RectTransform>();
+
+		float currentVelocity = 0f;
+		while (rectTransform.rect.height < DROPDOWN_HEIGHT - TOLERANCE)
+		{
+			float curHeight = rectTransform.rect.height;
+			curHeight = Mathf.SmoothDamp(curHeight, DROPDOWN_HEIGHT, ref currentVelocity, 0.4f);
+			rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, curHeight);
+			yield return null;
+		}
+
+		isInteractable = true;
+	}
+
+	IEnumerator DoCloseDropdown()
+	{
+		isInteractable = false;
+
+		RectTransform rectTransform = dropdownPanel.GetComponent<RectTransform>();
+
+		float currentVelocity = 0f;
+		while (rectTransform.rect.height > 0 + TOLERANCE)
+		{
+			float curHeight = rectTransform.rect.height;
+			curHeight = Mathf.SmoothDamp(curHeight, 0, ref currentVelocity, 0.4f);
+			rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, curHeight);
+			yield return null;
+		}
+
+		dropdownPanel.SetActive(false);
+		isInteractable = true;
+	}
+}
