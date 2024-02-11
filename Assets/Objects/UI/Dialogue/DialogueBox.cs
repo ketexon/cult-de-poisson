2bffa8f4310614867c936ecab6a62ad342fd5217
Schuyler Henry <schenry823@gmail.com>
2024-02-10 22:30:42 -0800

    NPC Dialogue Box Integrated with Yarnspinner
    Dialogue Options not yet implemented

--- Assets/Objects/UI/Dialogue/DialogueBox.cs
+++ Assets/Objects/UI/Dialogue/DialogueBox.cs
@@ -1,14 +1,15 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Animations;
+using Yarn.Unity;
 
-public class DialogueBox : MonoBehaviour
+public class DialogueBox : DialogueViewBase
 {
     [SerializeField] TMP_Text textComponent;
 
-
     Player player;
+    float textSpeed = 0.05f;
 
     void Start()
     {
@@ -20,15 +21,11 @@ public class DialogueBox : MonoBehaviour
         LookAtPlayer();
     }
 
-    public void UpdateText(string text)
-    {
-        textComponent.text = "";
-        StartCoroutine(DoUpdateText(text));
-    }
-
     void LookAtPlayer()
     {
-        Quaternion target = Quaternion.LookRotation(player.transform.position - transform.position);
+        Vector3 playerTarget = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+
+        Quaternion target = Quaternion.LookRotation(playerTarget - transform.position);
         Quaternion current = transform.parent.rotation;
 
         float angle = Mathf.Max(Quaternion.Angle(current, target), 15f);
@@ -36,12 +33,33 @@ public class DialogueBox : MonoBehaviour
         transform.parent.rotation = Quaternion.RotateTowards(current, target, angle * Time.deltaTime);
     }
 
-    IEnumerator DoUpdateText(string text)
+    IEnumerator RunLineInternal(string text, Action onDialogueLineFinished)
     {
         foreach (char c in text)
         {
             textComponent.text += c;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(textSpeed);
         }
+
+        yield return new WaitForSeconds(3f);
+
+        onDialogueLineFinished?.Invoke();
+    }
+
+    public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
+    {
+        textSpeed = 0.05f;
+        StartCoroutine(RunLineInternal(dialogueLine.RawText, onDialogueLineFinished));
+    }
+
+    public override void InterruptLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
+    {
+        textSpeed = 0.01f;
+    }
+
+    public override void DismissLine(Action onDismissalComplete)
+    {
+        textComponent.text = "";
+        onDismissalComplete?.Invoke();
     }
 }
