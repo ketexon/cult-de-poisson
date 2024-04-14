7e6ae8e93c05c1a3462b6e64e8bd40b1d79a9a46
ketexon <zane.a.s.clark@gmail.com>
2024-04-13 18:44:40 -0700

    fixed pixellation to work on all aspect ratios

---
+++ Assets/Objects/UI/CameraPixellater.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class CameraPixellater : MonoBehaviour
+{
+    [SerializeField] new Camera camera;
+    [SerializeField] RawImage image;
+    [SerializeField] int minWidth = 640;
+    [SerializeField] int minHeight = 480;
+
+    float MinAspect => (float) minWidth / minHeight;
+
+    RenderTexture tex;
+
+    UniversalAdditionalCameraData urpCamera;
+
+    float lastAspect;
+
+    void Start()
+    {
+        RecreateRenderTexture();
+    }
+
+    void OnDestroy()
+    {
+        if (tex)
+        {
+            tex.Release();
+        }
+    }
+
+    void Update()
+    {
+        var screenAspect = (float)Screen.width / Screen.height;
+        if(!Mathf.Approximately(screenAspect, lastAspect))
+        {
+            RecreateRenderTexture();
+        }
+    }
+
+    void RecreateRenderTexture()
+    {
+        var screenAspect = (float) Screen.width / Screen.height;
+        int actualWidth;
+        int actualHeight;
+        if (screenAspect * minHeight > minWidth)
+        {
+            actualHeight = minHeight;
+            actualWidth = (int) (screenAspect * minHeight);
+        }
+        else
+        {
+            actualHeight = (int)(screenAspect / minWidth); ;
+            actualWidth = minWidth;
+        }
+
+        if (tex)
+        {
+            tex.Release();
+        }
+
+        tex = new(actualWidth, actualHeight, 16);
+        tex.Create();
+
+        lastAspect = screenAspect;
+
+        camera.targetTexture = tex;
+        image.texture = tex;
+    }
+}
