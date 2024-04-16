226a2227c0ffaebf11b44a7fb784ceff2e1a6266
prestonfutaba <112904882+prestonfutaba@users.noreply.github.com>
2024-04-15 20:18:34 -0700

    jellyfish working

---
+++ Assets/Objects/Fish/Jellyfish/JellyfishItemBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyfishItemBehaviour : FishItemBehaviour
+{
+    [SerializeField] Vector3 targetPosition = new(0, 0, 1);
+    [SerializeField] float transitionSpeed = 10;
+
+    public override bool TargetInteractVisible => true;
+    public override string TargetInteractMessage => turnedOn ? "Turn off Pufferfish :(" : "Turn on Pufferfish uwo";
+
+    bool turnedOn = false;
+
+
+    Vector3 initialPosition;
+
+    
+    void Start()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    public override void OnInteract()
+    {
+        turnedOn = !turnedOn;
+
+        InteractivityChangeEvent?.Invoke(this);
+    }
+
+    void Update()
+    {
+        var actualTarget = 
+            turnedOn
+                ? targetPosition - transform.parent.localPosition
+                : initialPosition;
+        
+        transform.localPosition = Vector3.Lerp(
+            transform.localPosition,
+            actualTarget,
+            Time.deltaTime * transitionSpeed
+        );
+    }
+}
