3ee7e6fb4e5ca83a70e95861158e29ee866d650d
Mark Zhang <markzhang@ucla.edu>
2024-05-06 18:49:19 -0700

    Implemented picking up the fish.

--- Assets/Objects/Fish/FishInteractable.cs
+++ Assets/Objects/Fish/FishInteractable.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class FishInteractable : Interactable
 {
     [SerializeField] string InteractMessageTemplate = "Pick up {0}";
+    [SerializeField] PlayerInventorySO inventorySO;
     private string fishName;
+    FishSO fishSO;
 
     public void Awake()
     {
         Fish temp = GetComponent<Fish>();
-        fishName = temp.FishSO.Name;
+
+        fishSO = temp.FishSO;
+        fishName = fishSO.Name;
 
     }
+    public override void OnInteract()
+    {
+        PickUpFish();            
+    }
+
+    public void PickUpFish()
+    {
+        // Add fish to player inventory
+        inventorySO.AddFish(fishSO);
+        // Destroy fish object
+        Destroy(gameObject);
+    }
+
     public override string TargetInteractMessage => string.Format(InteractMessageTemplate, fishName);
 }
