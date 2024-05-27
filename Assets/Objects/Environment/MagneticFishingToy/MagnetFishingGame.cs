1f180512459dec4efca4440e8bfe67ac197ea10e
Amy He <amyh11325@gmail.com>
2024-05-27 16:18:22 -0700

    make rod move down based on y offset instead of time, make rod move left and right

--- Assets/Objects/Environment/MagneticFishingToy/MagnetFishingGame.cs
+++ Assets/Objects/Environment/MagneticFishingToy/MagnetFishingGame.cs
@@ -6,14 +6,24 @@ using UnityEngine.InputSystem;
 public class MagnetFishingGame : MonoBehaviour
 {
     [SerializeField] InputActionReference activate;
-    [SerializeField] float rodAnimationDuration;
+    [SerializeField] InputActionReference move;
+    [SerializeField] float rodTipYOffset;
     private Transform turntable;
     private Transform rod;
+    private Transform rodTip;
+    private float rodInitialY;
+    private float rodHorizontalRotateSpeed;
     void Start()
     {
         activate.action.performed += OnClick;
+        move.action.performed += (InputAction.CallbackContext ctx) => {rodHorizontalRotateSpeed = ctx.ReadValue<Vector2>().x;};
+        move.action.canceled += (InputAction.CallbackContext ctx) => {rodHorizontalRotateSpeed = 0;};
+
         turntable = transform.Find("Turntable");
         rod = transform.Find("MagnetRodPivot");
+        rodTip = rod.Find("MagnetRodTip");
+        rodInitialY = rodTip.position.y;
+        rodHorizontalRotateSpeed = 0;
     }
 
     // Update is called once per frame
@@ -21,6 +31,7 @@ public class MagnetFishingGame : MonoBehaviour
     {
         Vector3 turntable_center = turntable.GetComponent<Renderer>().bounds.center;
         turntable.RotateAround(turntable_center, Vector3.up, 1.0f);
+        rod.Rotate(0f, rodHorizontalRotateSpeed * 0.2f, 0f, Space.Self);
         // turntable.Rotate(0, 1.0f, 0);
     }
 
@@ -32,23 +43,20 @@ public class MagnetFishingGame : MonoBehaviour
     // Get Fiiiiish
     IEnumerator Fish() {
         // Rotate Down
-        float timeElapsed = 0;
-        while (timeElapsed < rodAnimationDuration) {
-            rod.Rotate(0.7f, 0f, 0f, Space.Self);
-            timeElapsed += Time.deltaTime;
+        Debug.Log("Fiiiiiish");
+        while (rodInitialY - rodTip.position.y < rodTipYOffset) {
+            rod.Rotate(0.4f, 0f, 0f, Space.Self);
             yield return null;
         }
         // Wait
-        timeElapsed = 0;
+        float timeElapsed = 0;
         while (timeElapsed < 0.2f) {
             timeElapsed += Time.deltaTime;
             yield return null;
         }
         // Rotate Up
-        timeElapsed = 0;
-        while (timeElapsed < rodAnimationDuration) {
+        while (rodInitialY - rodTip.position.y > 0) {
             rod.Rotate(-0.7f, 0f, 0f, Space.Self);
-            timeElapsed += Time.deltaTime;
             yield return null;
         }
         activate.action.performed += OnClick;
