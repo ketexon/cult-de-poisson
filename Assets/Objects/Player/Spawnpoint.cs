0cec57e4a1cf5b7359dce29adf4406e8a3ab0188
ketexon <zane.a.s.clark@gmail.com>
2024-06-02 19:32:58 -0700

    fixup! added teleport functionality

--- Assets/Objects/Player/Spawnpoint.cs
+++ Assets/Objects/Player/Spawnpoint.cs
@@ -6,15 +6,4 @@ public class Spawnpoint : MonoBehaviour
 {
     [SerializeField] public Transform LookRoot;
     [SerializeField] public Transform Camera;
-
-    void Start()
-    {
-        IEnumerator Coro()
-        {
-            yield return new WaitForSeconds(5);
-            Player.Instance.Movement.Teleport(this);
-        }
-
-        StartCoroutine(Coro());
-    }
 }
